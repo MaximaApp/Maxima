477d8b9c9dbcaf623afa1671556868dbec5db56e
MaximaApp <noah.t.ma@gmail.com>
2026-02-19 18:56:10 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,37 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('maxima-v1').then((cache) => cache.addAll([
-      './',
-      './index.html',
-      './manifest.json'
-    ]))
+const CACHE_NAME = 'maxima-bolt-v1'; // Change 'v1' to 'v2' next time you update code
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-bolt.png'
+];
+
+// Install Service Worker and cache assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate and remove old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+// Fetch strategy: Try network first, fall back to cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
   );
 });
